9189f60f0ce11282867f86808372bb30e49ba294
Nabeel Ali <mail2nabeelali@gmail.com>
2024-07-21 10:54:11 +0500

    packages update

--- aliveembed_vx.js
+++ aliveembed_vx.js
@@ -5,8 +5,8 @@
     const displayStyle = scriptTag.getAttribute('data-display') || 'bottom-right';
     const showFab = scriptTag.getAttribute('data-show-fab') !== 'false';
     const iconSvg = scriptTag.getAttribute('data-icon-svg') || '<svg fill="#000000" width="800px" height="800px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><path d="M50,22 L50,22 C66.6,22 80,34.5 80.1,50 C80.1,65.5 66.7,78 50.1,78 C44.3,78 38.9,76.5 34.3,73.8 C33.8,73.5 33.2,73.4 32.6,73.6 L23.8,76.7 C22.2,77.2 20.7,75.8 21.2,74.2 L24,65.3 C24.2,64.8 24.1,64.2 23.8,63.7 C21.4,59.6 20,55 20,50 C20,34.5 33.4,22 50,22 Z M63,45 C63.6,45 64,44.6 64,44 L64,38 C64,37.4 63.6,37 63,37 L37,37 C36.4,37 36,37.4 36,38 L36,44 C36,44.6 36.4,45 37,45 L63,45 Z M43,65 C43.6,65 44,64.6 44,64 L44,52 C44,51.4 43.6,51 43,51 L37,51 C36.4,51 36,51.4 36,52 L36,64 C36,64.6 36.4,65 37,65 L43,65 Z M63,65 C63.6,65 64,64.6 64,64 L64,52 C64,51.4 63.6,51 63,51 L51,51 C50.4,51 50,51.4 50,52 L50,64 C50,64.6 50.4,65 51,65 L63,65 Z"/></svg>';
-    const popupWidth = scriptTag.getAttribute('data-popup-width') || '400px';
-    const popupHeight = scriptTag.getAttribute('data-popup-height') || '600px';
+    const popupWidth = scriptTag.getAttribute('data-popup-width') || '90vw';
+    const popupHeight = scriptTag.getAttribute('data-popup-height') || '80vh';
     const maxPopupWidth = scriptTag.getAttribute('data-max-popup-width') || '400px';
     const popupBottom = scriptTag.getAttribute('data-popup-bottom') || '20px';
     const popupRight = scriptTag.getAttribute('data-popup-right') || '20px';
@@ -111,4 +111,49 @@
 
     window.aliveFormIds = window.aliveFormIds || [];
     window.aliveFormIds.push(formContainer.id);
+
+    function applyResponsiveStyles() {
+        const width = window.innerWidth;
+
+        if (width <= 480) {
+            formContainer.style.width = '100vw';
+            formContainer.style.height = '100vh';
+            formContainer.style.bottom = '0';
+            formContainer.style.right = '0';
+
+            if (showFab) {
+                fabButton.style.width = '35px';
+                fabButton.style.height = '35px';
+                fabButton.style.bottom = '10px';
+                fabButton.style.right = '10px';
+            }
+        } else if (width <= 768) {
+            formContainer.style.width = '95vw';
+            formContainer.style.height = '90vh';
+            formContainer.style.bottom = '10px';
+            formContainer.style.right = '10px';
+
+            if (showFab) {
+                fabButton.style.width = '40px';
+                fabButton.style.height = '40px';
+                fabButton.style.bottom = '15px';
+                fabButton.style.right = '15px';
+            }
+        } else {
+            formContainer.style.width = popupWidth;
+            formContainer.style.height = popupHeight;
+            formContainer.style.bottom = popupBottom;
+            formContainer.style.right = popupRight;
+
+            if (showFab) {
+                fabButton.style.width = fabSize;
+                fabButton.style.height = fabSize;
+                fabButton.style.bottom = fabBottom;
+                fabButton.style.right = fabRight;
+            }
+        }
+    }
+
+    window.addEventListener('resize', applyResponsiveStyles);
+    applyResponsiveStyles();
 })();
